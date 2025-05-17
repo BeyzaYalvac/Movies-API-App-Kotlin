5549430a3b58c2e7074383ca1830cf504d59dcea
beyza.yalvac <beyza.yalvac03@gmail.com>
2025-05-17 17:32:57 +0300

    upload project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,13 +41,35 @@ android {
 
 dependencies {
 
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    //implementation(libs.androidx.recyclerview)
+    //kapt("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation ("io.reactivex.rxjava2:rxjava:2.2.7")
+    implementation ("io.reactivex.rxjava2:rxandroid:2.1.1")
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.google.code.gson:gson:2.11.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.paging:paging-runtime:2.1.0")
+    implementation("androidx.appcompat:appcompat:1.1.0-alpha02")
+    implementation("androidx.core:core-ktx:1.1.0-alpha04")
+    implementation("androidx.appcompat:appcompat:1.1.0-alpha02")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
